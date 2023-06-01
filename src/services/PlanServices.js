2fc382da791c4382fde0839faf9b5a998d778f93
Narendra539 <133186603+Narendra539@users.noreply.github.com>
2023-06-01 13:37:02 -0500

    chnages in plan

--- src/services/PlanServices.js
+++ src/services/PlanServices.js
@@ -2,18 +2,18 @@ import apiClient from "./services";
 
 export default {
   getPlans() {
-    return apiClient.get("plans");
+    return apiClient.get("itenararies");
   },
   getPlan(id) {
-    return apiClient.get("plans/" + id);
+    return apiClient.get("itenararies/" + id);
   },
   addPlan(plan) {
-    return apiClient.post("plans", plan);
+    return apiClient.post("itenararies", plan);
   },
   updatePlan(plan) {
-    return apiClient.put("plans/" + plan.id, plan);
+    return apiClient.put("itenararies/" + plan.id, plan);
   },
   deletePlan(planId) {
-    return apiClient.delete("plans/" + planId);
+    return apiClient.delete("itenararies/" + planId);
   },
 };
