9c91c2a1fd5ffda45c98a88b3a097fa66b80d984
Narendra539 <133186603+Narendra539@users.noreply.github.com>
2023-06-06 15:00:23 -0400

    add booking component

---
+++ src/views/Booking.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
+import BookingServices from "../services/BookingServices.js";
+import { ref } from "vue";
+import Spinner from "../components/Spinner.vue";
+import { getImageUrl,getPlanUrl } from "../global.js";
+
+
+const bookings = ref([]);
+const spinner = ref(true);
+const user = ref(null);
+onMounted(async () => {
+  user.value = JSON.parse(localStorage.getItem("user"));
+  await getBookings();
+  spinner.value = false;
+});
+
+async function getBookings() {
+  await BookingServices.getBookingsOfUser(user.value.id)
+    .then((res) => {
+      bookings.value = res.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+</script>
+
+<template>
+<v-container>
+  <div class="container" style="margin-top: 20px">
+    <div style="display: flex; justify-content: center;">
+      <h3>Bookings</h3>
+    </div>
+    <br/>
+    <Spinner v-if="spinner" />
+    <div class="col-md-12 container elevation-4 bookings" v-else>
+      <div v-for="(booking,index) in bookings" :key="booking.id">
+        <div class="booking">
+          <p> {{index+1}}. Booked On <strong> {{ booking.createdAt.slice(0,10) }} </strong></p>
+          <p><a class="btn btn-warning" :href="getPlanUrl(booking.itineraryId)">Get Trip information</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br/>
+</v-container>
+
+</template>
+
+<style scoped>
+
+.btn {
+    color: white;
+    background-color: #80162B;
+}
+.bookings {
+  padding: 30px;
+}
+.booking {
+  margin-top: 30px;
+  width:100%;
+  border-bottom: 1px solid black;
+  padding:15px;
+  margin-left: 20px;
+}
+</style>
